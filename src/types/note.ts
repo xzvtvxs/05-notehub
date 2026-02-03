344f61ad73a8bef020d3871e2c73e3e208f72435
Ivan Fotin <xzvtvxs@gmail.com>
2026-02-03 19:24:19 +0200

    v1.1

--- src/types/note.ts
+++ src/types/note.ts
@@ -1,12 +1,10 @@
+export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+
 export interface Note {
+  id: number; 
   title: string;
   content: string;
   tag: NoteTag;
-  id: string;
+  createdAt: string;
+  updatedAt: string; 
 }
-
-export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
-
-export type NoteId = Note['id'];
-
-export type NotePost = Omit<Note, 'id'>;
